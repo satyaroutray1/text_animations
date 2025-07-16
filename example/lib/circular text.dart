ca1b1bd4a9957a58e2d56a003634be9ef4e5a4be
satya <sroutraykec@gmail.com>
2025-07-16 20:01:38 +0530

    Circular Text

--- example/lib/circular text.dart
+++ example/lib/circular text.dart
@@ -10,8 +10,27 @@ class CircularText extends StatefulWidget {
   State<CircularText> createState() => _CircularTextState();
 }
 
-class _CircularTextState extends State<CircularText>{
+class _CircularTextState extends State<CircularText> with SingleTickerProviderStateMixin{
 
+  AnimationController? _controller;
+  Animation<double>? _animation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 10));
+    _animation = Tween<double>(begin: -pi/2, end: 2*pi).animate(_controller!)..addListener((){
+      setState(() {
+
+      });
+    });
+    _controller?.repeat();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
   @override
   Widget build(BuildContext context){
     return
@@ -22,12 +41,13 @@ class _CircularTextState extends State<CircularText>{
             color: Colors.lightBlueAccent,
             borderRadius: BorderRadius.all(Radius.circular(100))
           ),
-        margin: EdgeInsets.all(150),
+        //margin: EdgeInsets.symmetric(horizontal: 10),
         child: ArcText(
-            radius: 90,
-            text: 'Hello There, You!Hello There, YouHello There, You',
-            textStyle: TextStyle(fontSize: 18, color: Colors.black),
-            startAngle: -pi/2,
+            radius: 85,
+            text: 'Hello There,You!! Flutter!',
+            textStyle: TextStyle(fontSize: 24, color: Colors.black,
+            fontWeight: FontWeight.bold, letterSpacing: 5),
+            startAngle: _animation!.value,
             startAngleAlignment: StartAngleAlignment.start,
             placement: Placement.middle,
             direction: Direction.clockwise
